4e382ef7c563fc3b04fe58ee7b2a084386915b57
haijin <haijin736@google.com>
2025-11-17 02:11:25 +0900

    resolve config

--- cx-agent-react/src/components/AuthWrapper.tsx
+++ cx-agent-react/src/components/AuthWrapper.tsx
@@ -8,9 +8,9 @@ import { Amplify } from 'aws-amplify';
 const amplifyConfig = {
   Auth: {
     Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID, 
-      userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
-      region: process.env.NEXT_PUBLIC_AWS_REGION,
+      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID || '', 
+      userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID || '',
+      region: process.env.NEXT_PUBLIC_AWS_REGION || '',
       loginWith: {
         email: true,
       },
