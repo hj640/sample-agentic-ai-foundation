a9b7642a022cf78a29d8544b57545f69b95ccc10
haijin <haijin736@google.com>
2025-11-17 02:16:34 +0900

    resolve config

--- cx-agent-react/next.config.js
+++ cx-agent-react/next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_USER_POOL_ID: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    NEXT_PUBLIC_USER_POOL_CLIENT_ID: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
+    NEXT_PUBLIC_AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
   }
 }
 
--- cx-agent-react/src/config/amplify.ts
+++ cx-agent-react/src/config/amplify.ts
@@ -4,7 +4,6 @@ import { Amplify } from 'aws-amplify';
 const config = {
   userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
   userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
-  identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
   region: process.env.NEXT_PUBLIC_AWS_REGION,
   apiUrl: process.env.NEXT_PUBLIC_API_URL
 };
